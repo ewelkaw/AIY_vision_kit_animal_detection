898f1545698fd9b6310ee7e8b7be73b629cb8052
Ewelina Szmielew <ewelina@szmielew.pl>
2022-01-10 23:19:08 +0100

    Code cleaning

--- animals_detector.py
+++ animals_detector.py
@@ -10,6 +10,40 @@ from aiy.vision.inference import CameraInference
 from aiy.vision.models import object_detection
 from picamera import PiCamera
 
+BUZZER_GPIO = 22
+
+
+def make_push_notification():
+    requests.post(
+        "https://api.pushover.net/1/messages.json",
+        data={
+            "token": os.environ("TOKEN"),
+            "user": os.environ("USER"),
+            "message": "Animal detected",
+        },
+        files={
+            "attachment": (
+                "image.jpg",
+                open("image.jpg", "rb"),
+                "image/jpeg",
+            )
+        },
+    )
+
+
+def play_tone():
+    player = TonePlayer(gpio=BUZZER_GPIO, bpm=10)
+    player.play("E6q")
+    player.play("C6q")
+
+
+def use_leds():
+    with Leds() as leds:
+        leds.update(Leds.rgb_on(Color.RED))
+        time.sleep(1)
+        leds.update(Leds.rgb_off())
+        time.sleep(1)
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -23,8 +57,6 @@ def main():
         help="Detection probability threshold.",
     )
     args = parser.parse_args()
-    BUZZER_GPIO = 22
-    player = TonePlayer(gpio=BUZZER_GPIO, bpm=10)
 
     with PiCamera() as camera:
         # Configure camera
@@ -34,35 +66,15 @@ def main():
         # Do inference on VisionBonnet
         with CameraInference(object_detection.model()) as inference:
             for result in inference.run():
-
                 objects = object_detection.get_objects(result, args.threshold)
 
-                for i, obj in enumerate(objects):
-                    print("Object #%d: %s" % (i, obj))
+                for obj in objects:
+                    print("Object was detected: %s" % (obj))
                     if obj.kind in [2, 3]:
                         camera.capture("image.jpg")
-                        requests.post(
-                            "https://api.pushover.net/1/messages.json",
-                            data={
-                                "token": os.environ("TOKEN"),
-                                "user": os.environ("USER"),
-                                "message": "Animal detected",
-                            },
-                            files={
-                                "attachment": (
-                                    "image.jpg",
-                                    open("image.jpg", "rb"),
-                                    "image/jpeg",
-                                )
-                            },
-                        )
-                        player.play("E6q")
-                        player.play("C6q")
-                        with Leds() as leds:
-                            leds.update(Leds.rgb_on(Color.RED))
-                            time.sleep(1)
-                            leds.update(Leds.rgb_off())
-                            time.sleep(1)
+                        make_push_notification()
+                        play_tone()
+                        use_leds()
 
 
 if __name__ == "__main__":
